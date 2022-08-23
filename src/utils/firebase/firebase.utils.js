f3682168e2bdc1bce325a664ef5333128bc7f549
Kevin Royer <royerkevin@outlook.com>
2022-08-23 16:27:10 -0500

    added our sign in component

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,62 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCibTSDveDJxMPzehIv_E_OicEo4iEBf1o",
+    authDomain: "crwn-clothing-db-8ef9a.firebaseapp.com",
+    projectId: "crwn-clothing-db-8ef9a",
+    storageBucket: "crwn-clothing-db-8ef9a.appspot.com",
+    messagingSenderId: "635853202449",
+    appId: "1:635853202449:web:f9ef41d471823d8cbd7db0"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+    prompt: "select_account"
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    //see if there is an existing document reference
+    //doc() just returns a document reference, not the acutal data
+    const userDocRef = doc(db, 'users', userAuth.uid);
+
+    console.log(userDocRef)
+
+    //get the actual data from the object with getDoc
+    const userSnapShot = await getDoc(userDocRef);
+  
+
+    //check if user data exists
+    if (!userSnapShot.exists()) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            });
+        }
+        catch (error) {
+            console.log('error creating the user', error.message)
+        }
+    }
+
+    return userDocRef;
+
+
+
+    //if user data doesn't exist, create and set the document with the data from userAuth in my collection
+
+    //return userDocRef if it exists
+}
